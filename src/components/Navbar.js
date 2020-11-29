04aaf76b1ec1ce0aa35b7f94615af511dd7d15ab
samusing <55567829+samusing@users.noreply.github.com>
2020-11-29 18:10:12 -0500

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
 import { Button } from './Button';
 import './Navbar.css';
@@ -23,14 +23,20 @@ function Navbar() {
         }
     };
 
+    //Only render "Let's Chat" button once and not every time the page gets refreshed
+    useEffect(() => {
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    SM <i className='fas fa-female' />
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                    SM
+                    {/* SM  <i className='fas fa-female' /> */}
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -46,11 +52,11 @@ function Navbar() {
                             Projects
                         </Link>
                     </li>
-                    <li className='nav-item'>
+                    {/* <li className='nav-item'>
                         <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
                             Let's Chat!
                         </Link>
-                    </li>
+                    </li> */}
                 </ul>
                 {button && <Button buttonStyle='btn--outline'>Let's Chat</Button>}
             </div>
