6f29c501b486b05b46ca6bdbb5e02d4680e549d6
samusing <55567829+samusing@users.noreply.github.com>
2020-11-29 23:24:31 -0500

    fix hero section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home}/>
+          <Route path='/projects' component={Projects}/>
         </Switch>
       </Router>
     </>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,9 +8,18 @@ function HeroSection() {
         <div className='hero-container'>
             <div className='hero-btns'>
                 <h1 className="myName">Stephanie Musing</h1>
-                <p className="valueProp">Hi! Nice to e-meet you. My name is Stephanie</p>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Get Started</Button>
+                <p className="valueProp"><b>A Management Engineer passionate about building impactful products with a focus on accessibility!</b></p>
             </div>
+            {/* <div className="description-btns">
+                <p className="description">I'm a Management Engineering student at the University of Waterloo who is 
+                    <b> passionate about technology and excited to build impactful products!</b> 
+                    I will be <b>graduating with a Bachelor of Applied Science degree in April 2021</b> and am 
+                    <b>seeking full-time Product Management opportunities</b>.
+                    During my degree, I have gained two years of valuable work experience through a co-op program in which 
+                    I have worked in a variety of industries including tech, healthcare, engineering consulting, and automotive.  
+                </p>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Get Started</Button>
+            </div> */}
         </div>
     )
 }
