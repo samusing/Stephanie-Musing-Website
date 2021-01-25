1baf6d2777711fe3955f453786f1cb87d57735c5
samusing <55567829+samusing@users.noreply.github.com>
2021-01-25 16:11:04 -0500

    changes made to hero banner

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,13 +2,23 @@ import React from 'react'
 import { Button } from './Button';
 import './HeroSection.css';
 import '../App.css';
+import logo from './headshot-new.png';
+
+console.log(logo);
 
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <div className='hero-btns'>
-                <h1 className="myName">Stephanie Musing</h1>
-                <p className="valueProp"><b>A Management Engineer passionate about building inclusive and impactful products!</b></p>
+            <div className='hero-wrapper'>
+                <ul className="hero-items">
+                    <div className="hero-text">
+                        <h1 className="myName">Stephanie <br></br> Musing</h1>
+                        <p className="valueProp">Curiousity . Passion . Happiness</p>
+                    </div>
+                    <div className="hero-image">
+                        <img className="headshot" src={logo} alt="My headshot"/>
+                    </div>
+                </ul>  
             </div>
             {/* <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Get Started</Button> */}
 
